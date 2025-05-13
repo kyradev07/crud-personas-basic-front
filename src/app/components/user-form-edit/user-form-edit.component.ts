7fc735ee7bf612117d7be03dbb99b7c053f2b6af
Juan T <juantapr@suramericana.com.co>
2025-05-13 14:16:12 -0500

    se implementa edición de usuario

--- src/app/components/user-form-edit/user-form-edit.component.ts
+++ src/app/components/user-form-edit/user-form-edit.component.ts
@@ -1,10 +1,10 @@
-import { Component, inject } from '@angular/core';
-import { UserService } from '../user.service';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { User } from '../model/user.interface';
+import {Component, inject} from '@angular/core';
+import {UserService} from '../user.service';
+import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
+import {User} from '../model/user.interface';
 import Swal from 'sweetalert2';
-import { ActivatedRoute } from '@angular/router';
-import { catchError, of, tap } from 'rxjs';
+import {ActivatedRoute} from '@angular/router';
+import {catchError, of, tap} from 'rxjs';
 
 @Component({
   selector: 'app-user-form-edit',
@@ -16,25 +16,7 @@ import { catchError, of, tap } from 'rxjs';
 export class UserFormEditComponent {
   private userService: UserService = inject(UserService);
   private readonly fb: FormBuilder = inject(FormBuilder);
-
-  constructor(private router: ActivatedRoute) {
-    this.router.params.subscribe(params => {
-      this.userService.findById(1).pipe(
-        tap(user => this.loadForm(user)),
-        catchError(err => of(err))
-      ).subscribe({
-        error: (err) => {
-          console.log(err);
-          Swal.fire({
-            icon: "error",
-            title: "Oops...",
-            text: err.error.message,
-          });
-        }
-      });
-    });
-
-  }
+  private idUser;
 
   userForm = this.fb.nonNullable.group({
     name: ['', [Validators.required, Validators.minLength(2)]],
@@ -42,26 +24,44 @@ export class UserFormEditComponent {
     age: [1, [Validators.required, Validators.min(1)]],
   });
 
-  loadForm(user: User) {
-    this.userForm.controls.name.setValue(user.name);
-    this.userForm.controls.lastname.setValue(user.lastname);
-    this.userForm.controls.age.setValue(user.age);
+  constructor(private router: ActivatedRoute) {
+    this.idUser = this.router.snapshot.paramMap.get('id') ?? 0;
+
+    this.userService.findById(+this.idUser).pipe(
+      tap(user => this.loadForm(user)),
+      catchError(err => of(err))
+    ).subscribe({
+      error: (err) => {
+        console.log(err);
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: err.error.message,
+        });
+      }
+    });
+
   }
 
 
+  loadForm(user: User) {
+    this.userForm.get('name')?.setValue(user.name);
+    this.userForm.get('lastname')?.setValue(user.lastname);
+    this.userForm.get('age')?.setValue(user.age);
+  }
 
   onSubmit() {
-    /*const user: User = {
+    const user: User = {
       name: this.userForm.controls.name.value,
       lastname: this.userForm.controls.lastname.value,
       age: this.userForm.controls.age.value,
     };
 
-    this.userService.updateUser(user).subscribe({
+    this.userService.updateUser(+this.idUser, user).subscribe({
       next: (user: User) => {
         Swal.fire({
           title: "Add!",
-          text: `User ${user.name} added successfully.`,
+          text: `User ${user.name} updated successfully.`,
           icon: "success"
         });
         this.userForm.reset();
@@ -70,10 +70,10 @@ export class UserFormEditComponent {
         Swal.fire({
           icon: "error",
           title: "Oops...",
-          text: "Something went wrong! " + err,
+          text: err.error.message,
         });
       }
-    })*/
+    })
   }
 
 }
